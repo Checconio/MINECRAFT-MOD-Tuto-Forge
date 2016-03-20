2041133672f98b414d5a0acea8490768be895f52
checconio84 <checconio84@gmail.com>
2016-03-20 16:55:31 +0100

    checconio84
    Mise à Jour

--- 1.7.10/src/main/java/com/mod/tuto/init/BlockMod.java
+++ 1.7.10/src/main/java/com/mod/tuto/init/BlockMod.java
@@ -3,6 +3,7 @@ package com.mod.tuto.init;
 import com.mod.tuto.Reference;
 import com.mod.tuto.blocks.BlockBasic;
 import com.mod.tuto.blocks.BlockFenceMod;
+import com.mod.tuto.blocks.BlockIsbrhMod;
 import com.mod.tuto.blocks.BlockStairsMod;
 
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -15,13 +16,14 @@ import net.minecraft.item.ItemStack;
 
 public class BlockMod
 {
-    public static Block block_test, block_stairs, block_fence;
+    public static Block block_test, block_stairs, block_fence, blockIsbrh;
     
     public static void init()
     {
         block_test = new BlockBasic(Material.rock, 2).setBlockName("block_test").setCreativeTab(CreativeTabs.tabBlock).setBlockTextureName(Reference.MOD_ID + ":block_test");
         block_stairs = new BlockStairsMod(BlockMod.block_test, 0).setBlockName("block_stairs").setCreativeTab(CreativeTabs.tabBlock).setLightLevel(1.0F);
         block_fence = new BlockFenceMod(Reference.MOD_ID + ":block_test", Material.rock).setBlockName("block_fence").setLightLevel(1.0F);
+        blockIsbrh = new BlockIsbrhMod(Material.rock).setBlockName("blockIsbrh").setBlockTextureName("glowstone");
     }
     
     public static void register()
@@ -29,6 +31,7 @@ public class BlockMod
         GameRegistry.registerBlock(block_test, block_test.getUnlocalizedName().substring(5));
         GameRegistry.registerBlock(block_stairs, block_stairs.getUnlocalizedName().substring(5));
         GameRegistry.registerBlock(block_fence, block_fence.getUnlocalizedName().substring(5));
+        GameRegistry.registerBlock(blockIsbrh, blockIsbrh.getUnlocalizedName().substring(5));
         
         
         GameRegistry.addRecipe(new ItemStack(BlockMod.block_test, 5), new Object[]{"###", "#F#", '#', Items.apple, 'F', Items.diamond});
--- 1.7.10/src/main/java/com/mod/tuto/proxy/ClientProxy.java
+++ 1.7.10/src/main/java/com/mod/tuto/proxy/ClientProxy.java
@@ -3,6 +3,7 @@ package com.mod.tuto.proxy;
 import com.mod.tuto.entity.EntityTuto;
 import com.mod.tuto.init.RenderMod;
 import com.mod.tuto.models.ModelTuto;
+import com.mod.tuto.renderer.RenderBlockIsbrh;
 import com.mod.tuto.renders.RenderTuto;
 
 import cpw.mods.fml.client.registry.RenderingRegistry;
@@ -10,9 +11,15 @@ import net.minecraft.client.Minecraft;
 
 public class ClientProxy extends CommonProxy
 {
+    public static int renderIsbrh;
+    
     @Override
     public void registerRenders()
     {
         RenderMod.init();
+        
+        //Block ISBRH
+        renderIsbrh = RenderingRegistry.getNextAvailableRenderId();
+        RenderingRegistry.registerBlockHandler(new RenderBlockIsbrh());
     }
 }
