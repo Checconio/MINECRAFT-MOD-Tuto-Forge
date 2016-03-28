72bf15a59bd0f40ca3eb34d7c22ad1701384d7f0
checconio84 <checconio84@gmail.com>
2016-03-28 16:31:13 +0200

    checconio84
    Mise à Jour

--- 1.7.10/src/main/java/com/mod/tuto/init/BlockMod.java
+++ 1.7.10/src/main/java/com/mod/tuto/init/BlockMod.java
@@ -5,6 +5,7 @@ import com.mod.tuto.blocks.BlockBasic;
 import com.mod.tuto.blocks.BlockFenceMod;
 import com.mod.tuto.blocks.BlockIsbrhMod;
 import com.mod.tuto.blocks.BlockStairsMod;
+import com.mod.tuto.blocks.BlockTesr;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
@@ -16,7 +17,7 @@ import net.minecraft.item.ItemStack;
 
 public class BlockMod
 {
-    public static Block block_test, block_stairs, block_fence, blockIsbrh;
+    public static Block block_test, block_stairs, block_fence, blockIsbrh, blockTesr;
     
     public static void init()
     {
@@ -24,6 +25,7 @@ public class BlockMod
         block_stairs = new BlockStairsMod(BlockMod.block_test, 0).setBlockName("block_stairs").setCreativeTab(CreativeTabs.tabBlock).setLightLevel(1.0F);
         block_fence = new BlockFenceMod(Reference.MOD_ID + ":block_test", Material.rock).setBlockName("block_fence").setLightLevel(1.0F);
         blockIsbrh = new BlockIsbrhMod(Material.rock).setBlockName("blockIsbrh").setBlockTextureName("glowstone");
+        blockTesr = new BlockTesr(Material.rock).setCreativeTab(CreativeTabs.tabBlock).setBlockName("blockTesr").setBlockTextureName("glowstone");
     }
     
     public static void register()
@@ -32,6 +34,7 @@ public class BlockMod
         GameRegistry.registerBlock(block_stairs, block_stairs.getUnlocalizedName().substring(5));
         GameRegistry.registerBlock(block_fence, block_fence.getUnlocalizedName().substring(5));
         GameRegistry.registerBlock(blockIsbrh, blockIsbrh.getUnlocalizedName().substring(5));
+        GameRegistry.registerBlock(blockTesr, blockTesr.getUnlocalizedName().substring(5));
         
         
         GameRegistry.addRecipe(new ItemStack(BlockMod.block_test, 5), new Object[]{"###", "#F#", '#', Items.apple, 'F', Items.diamond});
--- 1.7.10/src/main/java/com/mod/tuto/init/EntityMod.java
+++ 1.7.10/src/main/java/com/mod/tuto/init/EntityMod.java
@@ -1,8 +1,12 @@
 package com.mod.tuto.init;
 
+import com.mod.tuto.Reference;
 import com.mod.tuto.entity.EntityTuto;
 import com.mod.tuto.entity.EntityTuto2;
 import com.mod.tuto.handlers.EntityHandler;
+import com.mod.tuto.tileentity.TileEntityBlockTesr;
+
+import cpw.mods.fml.common.registry.GameRegistry;
 
 public class EntityMod
 {
@@ -10,5 +14,8 @@ public class EntityMod
     {
         EntityHandler.registerMonster(EntityTuto.class, "MobTuto");
         EntityHandler.registerAmbiants(EntityTuto2.class, "MobTuto2");
+        
+        //Block Tesr
+        GameRegistry.registerTileEntity(TileEntityBlockTesr.class, Reference.MOD_ID + ":blockTesr");
     }
 }
